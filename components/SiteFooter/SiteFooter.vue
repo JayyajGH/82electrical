dbe43e19f90d3cb351d5b005939bb802c538801d
Jason Harris <jayyaj@hotmail.com>
2022-09-14 19:32:07 +0100

    Make the footer full-width

--- components/SiteFooter/SiteFooter.vue
+++ components/SiteFooter/SiteFooter.vue
@@ -1,29 +1,30 @@
 <template>
-  <footer class="footer wrapper" id='contact'>
-    <div class="footer__contact u-margin-bottom--medium">
-      <h2 class="heading2 heading--dark">Contact Us</h2>
-      <address>
-        <ul>
-          <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="mailto:andrew@82electrical.co.uk">andrew@82electrical.co.uk</a></li>
-          <!-- Add an aria label so the phone number is read out correctly by a screen reader -->
-          <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="tel:+447813408135" aria-label="Phone us on 0 7 8 1 3. 4 0 8. 1 3 5">07813 408135</a></li>
-          <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="https://goo.gl/maps/w1DEYMjpH37A4bV96" target="_blank" rel="noopener noreferrer">Ambleside Avenue, Bristol BS10</a></li>
-        </ul>
-      </address>
-    </div>
-    <div class="footer__social u-margin-bottom--medium">
-      <h2 class="heading2 heading--dark">Social</h2>
-      <address>
-        <ul>
-          <li class="social__item"><a href="https://www.facebook.com/ElectricianforBristol" target="_blank" rel="noopener noreferrer" class="social__logo facebook__logo" title="82Electrical onFacebook"></a></li>
-          <li class="social__item"><a href="https://instagram.com/ElectricianforBristol" target="_blank" rel="noopener noreferrer" class="social__logo instagram__logo" title="82Electrical on Instagram"></a></li>
-        </ul>
-      </address>
-    </div>
+  <footer class="footer__container" id='contact'>
+    <div class="footer wrapper">
+      <div class="footer__contact u-margin-bottom--medium">
+        <h2 class="heading2 heading--dark">Contact Us</h2>
+        <address>
+          <ul>
+            <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="mailto:andrew@82electrical.co.uk">andrew@82electrical.co.uk</a></li>
+            <!-- Add an aria label so the phone number is read out correctly by a screen reader -->
+            <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="tel:+447813408135" aria-label="Phone us on 0 7 8 1 3. 4 0 8. 1 3 5">07813 408135</a></li>
+            <li class="u-margin-bottom--small"><a class="contact-item__anchor" href="https://goo.gl/maps/w1DEYMjpH37A4bV96" target="_blank" rel="noopener noreferrer">Ambleside Avenue, Bristol BS10</a></li>
+          </ul>
+        </address>
+      </div>
+      <div class="footer__social u-margin-bottom--medium">
+        <h2 class="heading2 heading--dark">Social</h2>
+        <address>
+          <ul>
+            <li class="social__item"><a href="https://www.facebook.com/ElectricianforBristol" target="_blank" rel="noopener noreferrer" class="social__logo facebook__logo" title="82Electrical onFacebook"></a></li>
+            <li class="social__item"><a href="https://instagram.com/ElectricianforBristol" target="_blank" rel="noopener noreferrer" class="social__logo instagram__logo" title="82Electrical on Instagram"></a></li>
+          </ul>
+        </address>
+      </div>
 
-    <div class="footer__copyright">
-      <!--<p class="smallest-text">Site designed and developed by <a href="https://stash74.co.uk">Stash74</a> © 2019</p>-->
-      <p class="smallest-text">Site developed by Jason Harris © 2022</p>
+      <div class="footer__copyright">
+        <p class="smallest-text">Site developed by Jason Harris © 2022</p>
+      </div>
     </div>
   </footer>
 </template>
@@ -35,6 +36,10 @@
 </script>
 
 <style scoped lang="scss">
+  .footer__container {
+    background-color: var(--grey-light);
+  }
+
   .footer {
     display: flex;
     flex-wrap: wrap;
